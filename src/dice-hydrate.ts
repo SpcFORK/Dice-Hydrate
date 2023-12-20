9710a07bc2469724e81b8f6c8b7c1f95ba189434
SpcFORK <126259962+SpcFORK@users.noreply.github.com>
2023-12-20 03:00:55 +0000

    Part 2

--- src/dice-hydrate.ts
+++ src/dice-hydrate.ts
@@ -29,18 +29,36 @@
 */
 
 const om_ = {
+  when(conditionFunction: Function, actionFunction: Function) {
+    return new Promise((resolve, reject) => {
+      if (typeof conditionFunction !== 'function') {
+        reject(new Error('Arg 1; MUST BE FUNCTION!!!'));
+        return
+      }
+
+      const interval = setInterval(() => {
+        if (conditionFunction()) {
+          clearInterval(interval);
+          actionFunction?.();
+          resolve(false);
+        }
+      }, 100); // Check the condition every 100 milliseconds.
+    });
+  },
+
   hydrateCache: {
     elements: [] as HTMLElement[],
-    currentPromise: null as Promise<boolean> | null,
+    currentPromise: null as Promise<HTMLIFrameElement> | null,
+    loaded: false,
   },
 
   listAllEventListeners() {
-    const 
+    const
       allElements = Array.prototype.slice.call(document.querySelectorAll('*')),
       types = []
-    
+
     allElements.push(document) // we also want document events
-    
+
     for (let ev in window) {
       if (/^on/.test(ev)) types[types.length] = ev;
     }
@@ -59,7 +77,7 @@ const om_ = {
       }
     }
 
-    return elements.sort(function(a,b) {
+    return elements.sort(function(a, b) {
       return a.type.localeCompare(b.type);
     });
   },
@@ -80,73 +98,101 @@ const om_ = {
 
     return events
   },
-  
+
   dice: document.querySelector<HTMLElement>('#crscrs'),
 
   LOC_: 'https://dicebutton.coding398.dev/?f=',
-  locate: (url: string) => om_.LOC_+url,
-  
+  locate: (url: string) => om_.LOC_ + url,
+
   sendEvent(ele: HTMLElement, name: string) {
     let e = new Event(name);
     ele.dispatchEvent(e);
   },
 
   run() {
-    let dice = om_.dice;
+    let dice = om_.dice
 
     if (!dice) return;
     // dice as HTMLElement;
+    let prd = dice.parentElement
 
-    // First, remove all events in dice event stack
-    let diceEvents = om_.findListenerEvents(dice);
+    if (!prd) return
 
-    for (let i = 0; i < diceEvents.length; i++) {
-      let currentEvent = diceEvents[i];
-      dice.removeEventListener(currentEvent.type, currentEvent.func);
-    }
+    let bb = dice.getBoundingClientRect();
 
+    let parentOfDice = dice.parentElement as HTMLElement;
+    // Put an overlay on the zfront which is ment to capture clicks
+    // let overlay = document.createElement('div');
+    // overlay.style.position = 'absolute';
+    // overlay.style.display = 'flex';
+    // overlay.style.width = '60px';
+    // overlay.style.height = '60px';
+    // overlay.style.bottom = '20px';
+    // overlay.style.left = '20px';
+    let overlay = dice.cloneNode(true) as HTMLElement;
+    overlay.style.zIndex = '2147483647';
+    dice.remove()
+    
+    document.body.appendChild(overlay);
+    om_.dice = overlay;
+    
     // Mouse over dice?
-    dice.addEventListener('mouseover', async (e) => {
-      e.preventDefault();
-      e.stopImmediatePropagation();
+    overlay.addEventListener('mouseover', async (e) => {
       await om_.hydrate()
     })
 
     // Mouse left dice?
-    dice.addEventListener('mouseleave', (e) => {
-      e.preventDefault();
-      e.stopImmediatePropagation();
-      om_.unhydrate()
+    overlay.addEventListener('mouseleave', async (e) => {
+      await om_.unhydrate()
     })
 
-    dice.addEventListener('click', async (e) => {
-      // Prevent link
-      e.preventDefault();
-      e.stopImmediatePropagation();
+    overlay.addEventListener('click', async (e) => {
       await om_.hydrateCache.currentPromise;
+      await om_.immerse()
+      om_.hydrateCache.loaded = false;
+      om_.hydrateCache.currentPromise = null;
     })
+
+
+    // let rule;
+    // om_.when(() => getComputedStyle(dice, '::after').content !== 'none', () => {
+    //   // Clone Pseudo Element
+    //   let pseudoElement = document.createElement('div');
+
+    //   overlay.appendChild(pseudoElement);
+    // })
+    // // parentOfDice.appendChild(overlay);
   },
 
   async hydrate() {
     let dice = om_.dice
+    if (om_.hydrateCache.loaded) return
 
     let iframe = document.createElement('iframe');
-    iframe.src = om_.locate(window.location.href);
+    iframe.src = om_.locate(document.location.host);
     iframe.style.display = 'none';
-    document.body.appendChild(iframe)
+    document.body.appendChild(iframe);
 
     // Wait for Iframe to load
     om_.hydrateCache.currentPromise = new Promise((resolve) => {
       iframe.onload = () => {
-        resolve(true);
+        resolve(iframe);
       }
     })
 
     await om_.hydrateCache.currentPromise;
+
+    om_.hydrateCache.loaded = true;
   },
 
-  unhydrate() {
+  async unhydrate() {
+    let elm = await om_.hydrateCache.currentPromise
+
+    if (om_.hydrateCache.loaded) return 
     
+    if (elm) {
+      elm.remove()
+    }
   },
 
   fullscreen(element: HTMLElement) {
@@ -161,6 +207,33 @@ const om_ = {
     element.style.border = 'none';
     element.style.outline = 'none';
     element.style.display = 'block';
+  },
+
+  async immerse() {
+    let curp = await om_.hydrateCache.currentPromise
+    if (!curp) return;
+
+    let allowedContent = typeof curp.contentDocument == typeof Document
+      ? curp.contentDocument
+      : false
+
+    if (allowedContent) {
+      let allElements = curp.querySelectorAll('*');
+      let scripts = allowedContent.scripts
+
+      let clonedNodes = [...allElements].map((element: Node) => element.cloneNode(true));
+
+      if (allElements) {
+        document.write('')
+        for (let i = 0; i < clonedNodes.length; i++) {
+          let currentElement = allElements[i];
+
+          document.appendChild(currentElement);
+        }
+      }
+    } else {
+      om_.fullscreen(curp)
+    }
   }
 }
 
